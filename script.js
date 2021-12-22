8d78c286649f3ee99c56092c2c2b48cd7b4801db
NawZin <zmarlarwin@gmail.com>
2021-12-23 01:24:26 +0900

    add 3 files

---
+++ script.js
@@ -0,0 +1,18 @@
+function compute(){
+var principal= document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years=document.getElementById("years").value;
+var year = new Date().getFullYear()+parseInt(years);
+
+
+if((principal)<="0"){
+    alert("Enter a positive number");
+    principal.focus();
+}
+var result =((principal*rate)/100)*years;
+document.getElementById("result").innerText = result;
+}
+function updateRate(){
+var rateval= document.getElementById("rate").value;
+document.getElementById("rate_val").innerText = rateval;
+ }
